2b69b06465125ff4448d71ce756c11546c28345a
megalamaggi <megala03.2002@gmail.com>
2022-11-18 17:03:16 +0530

    swaped TableHeader And TableRow.

--- src/components/Simpletable.js
+++ src/components/Simpletable.js
@@ -3,23 +3,8 @@ import { React } from 'react';
 import markSheets from '../service/markSheet';
 import headers from '../service/header';
 import addFields from '../service/studentsManager';
-
-const TableHeader = (data, key) =>
-	<th key={ key } className="headerStyle">{ data }</th>;
-
-const TableRow = ({ rollNo, name, tamil, english, maths, science, social, total, result, rank }, key) =>
-	<tr key={ key }>
-		<td className="numberStyle"> { rollNo}</td>
-		<td className="textStyle"> {name}</td>
-		<td className="numberStyle"> {tamil}</td>
-		<td className="numberStyle">{english}</td>
-		<td className="numberStyle">{maths}</td>
-		<td className="numberStyle">{science}</td>
-		<td className="numberStyle">{social}</td>
-		<td className="numberStyle">{total} </td>
-		<td className="numberStyle">{result} </td>
-		<td className="numberStyle">{rank} </td>
-	</tr>;
+import TableHeader from './TableHeader';
+import TableRow from './TableRow';
 
 const Simpletable = () =>
 	<div>
---
+++ src/components/TableHeader.js
@@ -0,0 +1,5 @@
+const TableHeader = (data, key) =>
+	// eslint-disable-next-line react/react-in-jsx-scope
+	<th key={ key } className="headerStyle">{ data }</th>;
+
+export default TableHeader;
